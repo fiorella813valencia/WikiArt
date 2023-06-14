0af9bc2e9040738c71e8ff330257a3c886c74114
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-06-14 11:07:41 -0500

    author model validation

--- src/main/java/com/example/wikiart/application/author/domain/model/Author.java
+++ src/main/java/com/example/wikiart/application/author/domain/model/Author.java
@@ -1,10 +1,12 @@
 package com.example.wikiart.application.author.domain.model;
 
 import com.example.wikiart.shared.domain.model.AuditModel;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 
 @Getter
 @Setter
@@ -12,15 +14,25 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="authors")
+@Table(name="author")
 public class Author extends AuditModel {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
+    @NotNull
     private String firstName;
+    @NotBlank
+    @NotNull
     private String lastName;
+    @NotBlank
+    @NotNull
     private String nickname;
+    @NotBlank
+    @NotNull
     private String photoUrl;
+
 
 
 
